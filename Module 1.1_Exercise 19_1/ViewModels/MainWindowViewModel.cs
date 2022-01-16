e35e203dfcc9fa82d6f7dac9ad1fce6da96491e8
nikit <nikit@DESKTOP-LGQ6VHQ>
2022-01-17 00:36:05 +0300

    Добавьте файлы проекта.

---
+++ Module 1.1_Exercise 19_1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Module_1._1_Exercise_19_1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Module_1._1_Exercise_19_1.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int radius;
+        public int Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double answer;
+        public double Answer
+        {
+            get => answer;
+            set
+            {
+                answer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Answer = Ariph.GetCircumference(Radius);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Radius!=0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+
+    }
+}
